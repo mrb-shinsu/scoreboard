a21b4a45867f93350251ef17524cad32a621e4c9
mrb-shinsu <bojovicm@sbb.rs>
2024-04-21 19:11:45 +0200

    Test storage exception

--- src/test/java/com/odds/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/odds/scoreboard/ScoreBoardTest.java
@@ -2,6 +2,7 @@ package com.odds.scoreboard;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class ScoreBoardTest {
@@ -9,7 +10,7 @@ public class ScoreBoardTest {
     private final MatchStorage matchStorage = mock(MatchStorage.class);
 
     @Test
-    public void startMatchAllValidSuccess() {
+    public void startMatchIfAllValidSuccess() {
         String homeTeam = "Mexico", awayTeam = "Canada";
 
         ScoreBoard sc = new ScoreBoard(matchStorage);
@@ -19,5 +20,15 @@ public class ScoreBoardTest {
         Match expectedMatch = new Match(homeTeam, 0, awayTeam, 0);
         verify(matchStorage, times(1)).
                 save(expectedKey, expectedMatch);
+    }
+
+    @Test
+    public void startMatchIfStorageExceptionFailure() {
+        String homeTeam = "Mexico", awayTeam = "Canada";
+        doThrow(RuntimeException.class).when(matchStorage)
+                .save(anyString(), any());
+
+        ScoreBoard sc = new ScoreBoard(matchStorage);
+        assertThrows(RuntimeException.class, () -> sc.startMatch(homeTeam, awayTeam));
     }
 }
