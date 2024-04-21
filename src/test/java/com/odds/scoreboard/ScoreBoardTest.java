48b78e5bd5938b9a75911085c5c103f33df6c7f5
mrb-shinsu <bojovicm@sbb.rs>
2024-04-21 20:16:17 +0200

    Test startMatch parameters validation

--- src/test/java/com/odds/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/odds/scoreboard/ScoreBoardTest.java
@@ -1,8 +1,11 @@
 package com.odds.scoreboard;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class ScoreBoardTest {
@@ -23,7 +26,7 @@ public class ScoreBoardTest {
     }
 
     @Test
-    public void startMatchIfStorageExceptionFailure() {
+    public void startMatchIfStorageExceptionThrowException() {
         String homeTeam = "Mexico", awayTeam = "Canada";
         doThrow(RuntimeException.class).when(matchStorage)
                 .save(anyString(), any());
@@ -31,4 +34,16 @@ public class ScoreBoardTest {
         ScoreBoard sc = new ScoreBoard(matchStorage);
         assertThrows(RuntimeException.class, () -> sc.startMatch(homeTeam, awayTeam));
     }
+
+    @ParameterizedTest
+    @CsvSource({",Canada", "'',Canada", "Mexico,", "Mexico,''", ",", ",''", "'',", "'',''"})
+    public void startMatchIfHomeAndAwayTeamNullOrEmptyThrowException(String homeTeam, String awayTeam) {
+        ScoreBoard sc = new ScoreBoard(matchStorage);
+        Exception e = assertThrows(RuntimeException.class, () -> sc.startMatch(homeTeam, awayTeam));
+
+        String expectedMessage = "Invalid input: Home/away team null or empty";
+        String actualMessage = e.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
 }
