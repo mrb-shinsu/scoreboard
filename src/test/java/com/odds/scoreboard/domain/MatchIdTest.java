e3fb0ef947196097e099ee786850e722b32c3765
mrb-shinsu <bojovicm@sbb.rs>
2024-04-24 23:50:50 +0200

    Additional tests

--- src/test/java/com/odds/scoreboard/domain/MatchIdTest.java
+++ src/test/java/com/odds/scoreboard/domain/MatchIdTest.java
@@ -1,11 +1,21 @@
 package com.odds.scoreboard.domain;
 
+import com.odds.scoreboard.BaseTest;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 
-class MatchIdTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatchIdTest extends BaseTest {
     @Test
     void equalsContract() {
         EqualsVerifier.simple().forClass(MatchId.class).verify();
+    }
+
+    @Test
+    void getIdCheckConstructor() {
+        var matchId = new MatchId(MEXICO, CANADA);
+
+        assertEquals(MEXICO + "_" + CANADA, matchId.getId());
     }
 }
--- src/test/java/com/odds/scoreboard/domain/MatchTest.java
+++ src/test/java/com/odds/scoreboard/domain/MatchTest.java
@@ -1,11 +1,28 @@
 package com.odds.scoreboard.domain;
 
+import com.odds.scoreboard.BaseTest;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 
-class MatchTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MatchTest extends BaseTest {
     @Test
     void equalsContract() {
         EqualsVerifier.simple().forClass(Match.class).verify();
+    }
+
+    @Test
+    void getTotalScoreIfZeroReturnZero() {
+        var match = new Match(MEXICO, 0, CANADA, 0);
+
+        assertEquals(0, match.getTotalScore());
+    }
+
+    @Test
+    void getTotalScoreIfGtZeroReturnTotal() {
+        var match = new Match(MEXICO, 1, CANADA, 7);
+
+        assertEquals(8, match.getTotalScore());
     }
 }
